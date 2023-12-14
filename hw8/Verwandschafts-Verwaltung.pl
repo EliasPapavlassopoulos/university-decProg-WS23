2091e04ef562690a37ab9696cab3e30dfaa3dab3
Elias Papavlassopoulos <e.papa@live.de>
2023-12-14 02:15:11 +0100

    Added definitions for grandmother, siblings, brother, and aunt

--- hw8/Verwandschafts-Verwaltung.pl
+++ hw8/Verwandschafts-Verwaltung.pl
@@ -1,3 +1,9 @@
+% Aufgabe war es, die Grossmutter, Geschwister, Bruder und Tante zu definieren.
+% Die Grossmutter kann gefunden werden, indem die Mutter der Mutter gefunden wird.
+% Geschwister koennen gefunden werden, indem die Kinder der gleichen Eltern gefunden werden, die nicht die gleiche Person sind.
+% Bruder kann gefunden werden, indem maennliche Geschwister der Person gefunden werden.
+% Tante kann gefunden werden, indem nach weiblichen Geschwistern der Mutter der Person gesucht wird.
+
 % Definition der Verheiratet-Relation:
 verheiratet(christine, heinz).
 verheiratet(hubert,    fritz).
@@ -37,18 +43,14 @@ vater(Kind,Vater) :- kind(Kind,_,Vater), geschlecht(Vater,m).
 mutter(Kind,Mutter) :- kind(Kind,Mutter,_), geschlecht(Mutter,w).
 mutter(Kind,Mutter) :- kind(Kind,_,Mutter), geschlecht(Mutter,w).
 
-
-
-% ABGABE & BEARBEITUNG
-
 % Grandmother can be found by finding the mother of the mother
 grossmutter(Person, Grossmutter) :-
-    mutter(Person,  Mutter),
-    mutter(Mutter,  Grossmutter).
+    mutter(Person, Mutter),
+    mutter(Mutter, Grossmutter).
 
 % Siblings can be found by finding the children of the same parents that are not the same person
-geschwister(Person,   Geschwister) :-
-    vater(Person,     Vater),
+geschwister(Person, Geschwister) :-
+    vater(Person, Vater),
     kind(Geschwister, Vater, _),
     Geschwister \= Person.
 
@@ -57,8 +59,8 @@ bruder(Person, Bruder) :-
     geschwister(Person, Bruder),
     geschlecht(Bruder, m).
 
-% Aunt can be found by searching for female siblings of persons mother
+% Aunt can be found by searching for female siblings of person's mother
 tante(Person, Tante) :-
     mutter(Person, Mutter),
     geschwister(Mutter, Tante),
-    geschlecht(Tante, w). 
+    geschlecht(Tante, w).
